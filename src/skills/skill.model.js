82ae76edfc13fcc7a6069060bbafab6d5de1dc62
Joselop18 <aaguilar-2023496@kinal.edu.gt>
2025-06-21 17:59:48 -0600

    Se realizo modelo Skills

---
+++ src/skills/skill.model.js
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+import autopopulate from 'mongoose-autopopulate';
+
+const skillSchema = new Schema(
+    {
+        nameSkill: {
+            type: String,
+            required: [true, "Skill name is required"],
+            trim: true,
+            maxLength: 100
+        },
+        levelSkill: {
+            type: String,
+            enum: ["BEGINNER", "INTERMEDIATE", "ADVANCED"],
+            default: "BEGINNER"
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+            autopopulate: true
+        },
+        createdDate: {
+            type: Date,
+            default: Date.now
+        },
+        stauts:{
+            type: String,
+            default: true
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+skillSchema.plugin(autopopulate);
+
+export default model('Skill', skillSchema);
